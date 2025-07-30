8754819b80c7f2af49782c2759454271f00206ee
aliraza5490 <ansarialiraza193@gmail.com>
2025-07-30 14:56:40 -0400

    Add inspirational-quotes dependency and update health route to return a quote

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from src.routes import api_router
 from src.settings import settings
 from tortoise.contrib.fastapi import tortoise_exception_handlers
 from pydantic import BaseModel
+from inspirational_quotes import quote
 
 app = FastAPI(title="FastAPI Template", description="API documentation", version="1.0.0", lifespan=lifespan, exception_handlers=tortoise_exception_handlers())
 
@@ -20,9 +21,11 @@ app.add_middleware(
 
 class Health(BaseModel): 
     message: str
+    quote: str
 
 @app.get("/", tags=["health"], response_model=Health, summary="Health Route", description="This is a health route for checking server status.")
 def read_root():
-    return {"message": "Hello World!"}
+    qt = quote()
+    return {"message": "Hello World!", "quote": qt.get("quote", "No quote available")}
 
 app.include_router(api_router)
